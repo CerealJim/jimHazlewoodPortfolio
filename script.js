4fcb90ebf3fbc23bf41b098ce0635ff83c18a019
Jim <jim.haz34@gmail.com>
2021-10-19 20:32:58 -0400

    headerStyles

---
+++ script.js
@@ -0,0 +1,11 @@
+// const navHamburger = document.querySelector(".hamburger")
+// const desktopNav = document.querySelector(".deskTopNav")
+
+// navHamburger.addEventListener("click", () => {
+//   navHamburger.classList.toggle("active");
+// })
+
+// document.querySelectorAll(".navItem").forEach("click",() => {
+//   navHamburger.classList.remove("active");
+//   desktopNav.classList.remove("active");
+// })
