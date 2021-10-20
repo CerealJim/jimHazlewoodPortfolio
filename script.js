2fd11303a1f296ac2448e42dacd7dfd69a63a7ec
Jim <jim.haz34@gmail.com>
2021-10-19 22:36:35 -0400

    flexing about and portfolio sections

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const navHamburger = document.querySelector(".hamburger")
 const navList = document.querySelector(".navList")
+const headerNav = document.querySelector(".headerNav")
 
 navHamburger.addEventListener("click", () => {
   navList.classList.toggle("active");
+  headerNav.classList.toggle("active");
+  navHamburger.classList.toggle("active");
 })
