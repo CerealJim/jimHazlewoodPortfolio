6db9cd91ceb6c477040c2b299bf78a0c23f212fb
Jim <jim.haz34@gmail.com>
2021-10-20 20:04:52 -0400

    styling portfolio section

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ const headerNav = document.querySelector(".headerNav")
 
 navHamburger.addEventListener("click", () => {
   navList.classList.toggle("active");
-  headerNav.classList.toggle("active");
-  navHamburger.classList.toggle("active");
+  // headerNav.classList.toggle("active");
+  // navHamburger.classList.toggle("active");
 })
 
 
 
-const iconSkill = document.querySelector(".icon")
-const iconName = document.querySelector("iconName")
+// const iconSkill = document.querySelector(".icon")
+// const iconName = document.querySelector("iconName")
 
-iconSkill.addEventListener("hover", () => {
-  console.log('hover is working')
-  iconName.classList.toggle("show");
-})
+// iconSkill.addEventListener("hover", () => {
+//   console.log('hover is working')
+//   iconName.classList.toggle("show");
+// })
