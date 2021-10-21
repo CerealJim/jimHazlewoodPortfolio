9a9eaed2c268017f33976f216135fcf8a1c68de1
Jim <jim.haz34@gmail.com>
2021-10-20 22:51:55 -0400

    updating portfolio content

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-const navHamburger = document.querySelector(".hamburger")
-const navList = document.querySelector(".navList")
-const headerNav = document.querySelector(".headerNav")
 
-navHamburger.addEventListener("click", () => {
-  navList.classList.toggle("active");
-  // headerNav.classList.toggle("active");
-  // navHamburger.classList.toggle("active");
-})
+//Global app object (store all Methods/variables/etc.)
+const app = {};
 
+// HTML properties
+app.navHamburger = document.querySelector(".hamburger")
+app.navList = document.querySelector(".navList")
+app.headerNav = document.querySelector(".headerNav")
 
 
-// const iconSkill = document.querySelector(".icon")
-// const iconName = document.querySelector("iconName")
+app.hamburgerClick = () => {
+  app.navHamburger.addEventListener("click", () => {
+    navList.classList.toggle("active");
+  })
+}
 
-// iconSkill.addEventListener("hover", () => {
-//   console.log('hover is working')
-//   iconName.classList.toggle("show");
-// })
+app.init = () => {
+  app.hamburgerClick();
+}
+
+// call app.init
+app.init();
