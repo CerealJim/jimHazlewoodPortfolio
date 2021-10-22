da67df961f4032455c22a6877814d74f6d9de860
Jim <jim.haz34@gmail.com>
2021-10-22 00:27:13 -0400

    updated form to send me an email directly

--- script.js
+++ script.js
@@ -1,26 +1,6 @@
 
-
-    // const firebaseConfig = {
-    //   apiKey: "AIzaSyDT7Gm1f1VHkdNnIFcAERKe7mZA3aWLCHA",
-    //   authDomain: "portfolio-ccdec.firebaseapp.com",
-    //   databaseURL: "https://portfolio-ccdec-default-rtdb.firebaseio.com",
-    //   projectId: "portfolio-ccdec",
-    //   storageBucket: "portfolio-ccdec.appspot.com",
-    //   messagingSenderId: "265314339658",
-    //   appId: "1:265314339658:web:bd3c7d5d8159f07fb44fb3"
-    // };
-
-    // const firebaseApp = initializeApp(firebaseConfig);
-    // const realtime = getDatabase(firebaseApp);
-    // const dbRef = ref(realtime);
-    // console.log(dbRef, "dbRef")
-
-
-
 //Global app object (store all Methods/variables/etc.)
 const app = {};
-
-
 
 // HTML properties
 // Nav properties
@@ -28,28 +8,14 @@ app.navHamburger = document.querySelector(".hamburger")
 app.navList = document.querySelector(".navList")
 app.navLink = document.querySelectorAll(".navLink")
 // Form properties
-app.formElement = document.querySelector('form');
-app.formResponse = document.querySelector(".submitParagraph");
+// app.formElement = document.querySelector('form');
+// app.formResponse = document.querySelector(".submitParagraph");
 
 app.contactMessages = [];
-
-// //FIREBASE FUNCTIONS
-//     // Grab our content from firebase
-//     dbRef.on('value', (data) => {
-//       const firebaseData = data.val()
-
-//       app.portfolioResponse = []
-
-//       // adding in new data as it is available.
-//       for(props in firebaseData){
-//         app.portfolioResponse.push(listItem)
-//       }
-//     })
 
 app.init = () => {
   app.hamburgerClick();
   app.linkClick();
-  app.submission();
 }
 
 // Navigation hamburger cick
@@ -68,37 +34,20 @@ app.linkClick = () => {
 )}
 
 // Form submit click
-app.submission = () => {
-  app.formElement.addEventListener("submit", (e) => {
-    e.preventDefault();
-    if (document.getElementById('comment').value.length > 0) {
-    let contactMessage = {
-      id: Date.now(),
-      name: document.getElementById('name').value,
-      email: document.getElementById('email').value,
-      message: document.getElementById('comment').value
-    }
-    app.contactMessages.push(contactMessage);
-    app.formElement.reset();
-    console.log(app.contactMessages, "contact messages");
-    app.formResponse.textContent = `✔ Thank you!`;
-  } else {
-    alert('please fill in the form');
-    console.log(app.contactMessages, "contact messages");
-  }
-  })
-}
-
-
+// app.submission = () => {
+//   app.formElement.addEventListener("submit", (e) => {
+//     e.preventDefault();
+//     app.formElement.reset();
+//     app.formResponse.textContent = `✔ Thank you!`;
+//   })
+// }
 
 // call app.init
 app.init();
 
 
-// You can also pass an optional settings object
-// below listed default settings
+// ANNIMATION BY "https://michalsnik.github.io/aos/"
 AOS.init({
-  // Global settings:
   disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
   startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
   initClassName: 'aos-init', // class applied after initialization
@@ -107,9 +56,6 @@ AOS.init({
   disableMutationObserver: false, // disables automatic mutations' detections (advanced)
   debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
   throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
-  
-
-  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
   offset: 120, // offset (in px) from the original trigger point
   delay: 0, // values from 0 to 3000, with step 50ms
   duration: 600, // values from 0 to 3000, with step 50ms
